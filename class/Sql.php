86df55a3511e7e8a3cee5becef316346436c34f9
everton-ods <soeblues4.eo@gmail.com>
2024-08-19 08:32:45 -0300

    update

--- class/Sql.php
+++ class/Sql.php
@@ -1,38 +1,32 @@
 <?php
-
 class Sql extends PDO {
     private $conn;
-    public function __construct() {
-        parent::__construct("mysql:dbname=projeto;host=localhost", "root", "");
+    private $dbname = "projeto";
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    public function __construct(){
+        $str = sprintf("mysql:dbname=%s;host=%s", $this->dbname, $this->host);
+        parent::__construct($str, $this->username, $this->password);
     }
-    private function setParametros($statement, $listaParametros = array()) {
-        
+    private function setParametros($statement, $listaParametros = array()){
         foreach($listaParametros as $key => $value) {
             $statement->setParametro($statement, $key, $value);
             $statement->bindParam($key, $value);
         }
 
     }
-
-    public function execquery($rawQuery, $parametros = array()) {
+    public function execquery($rawQuery, $parametros = array()){
         $stmt = $this->conn->prepare($rawQuery);
         $this->setParametros($stmt, $parametros);
         $stmt->execute();
         return $stmt;
     }
-
     public function select($rawQuery, $parametros = array()):array{
         $stmt = $this->query($rawQuery);
         $this->setParametros($stmt, $parametros);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-    public function getConn() {
-        return $this->conn;
-    }
-
-    public function setConn($conn) {
-        $this->conn = $conn;
     }
 
 
--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function ($class){
-    $filename = $class.".php";
+    $filename = "class".DIRECTORY_SEPARATOR.$class.".php";
     if (file_exists($filename)) {
         require_once($filename) ;
     }
